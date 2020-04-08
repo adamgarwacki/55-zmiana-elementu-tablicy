4f87e138356024d6094f6c2d1c1f102f389a0ff2
Adam Garwacki <adam.garwacki@gmail.com>
2020-04-08 14:20:41 +0200

    zmieniono funkcję tak, by zwracała tablicę zamiast poszczególnych liczb

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ function zmianaElementu(Arr) {
     for (i = 0; i < Arr.length; i++) {
         if (Arr[i] > 10) {
             Arr[i] = 5;
-            console.log(Arr[i]);
-        } else {
-            console.log(Arr[i]);
         }
     }
+    console.log(Arr);
 }
 
 let Arr = [3, 15, 14];
